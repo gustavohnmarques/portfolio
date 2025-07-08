f25f6c2a8636ca7b7f189cd1d41fca983fbec86e
Gustavo Marques <gustavohnmarques@gmail.com>
2025-07-08 15:57:50 -0300

    Feat: Estrutura inicial de arquivos

---
+++ src/src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import * as S from "./Home.styles";
+
+const Home: React.FC = () => {
+  return (
+    <S.Container>
+
+    </S.Container>
+  )
+}
+
+export default Home;
