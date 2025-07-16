f2c1167b72be845571d69923b1a1ae57ecbd5ed0
Gustavo Marques <gustavohnmarques@gmail.com>
2025-07-16 18:35:48 -0300

    refactor: Remoção da identificação automatica de idioma do navegador

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,6 @@
 
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
 
 import { en } from './locales/en'
 import { pt } from './locales/pt'
@@ -11,12 +10,11 @@ const resources = {
   pt: { translation: pt },
 }
 
-i18n
-  .use(LanguageDetector)
+i18n  
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'en',
+    fallbackLng: 'pt',
     interpolation: { escapeValue: false },
   })
 
